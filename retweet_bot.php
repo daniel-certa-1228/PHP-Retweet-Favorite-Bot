1090d9841a76006792772671e8da947640f9c12a
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-28 22:21:32 -0600

    app checks for current tweets and will not attampt to tweet or favorite duplicates

--- retweet_bot.php
+++ retweet_bot.php
@@ -9,24 +9,36 @@
     //connect to Twitter using credentials from env
     $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
     // var_dump($connection);
+
+    $already_tweeted = [];
+    $home_check = $connection->get("statuses/home_timeline", ["count" => 20]);
+
+    for ($i = 0; $i < count($home_check); $i++) {
+        $current_tweet_ids = (get_object_vars($home_check[$i]))["id"];
+        array_push($already_tweeted, $current_tweet_ids);
+    }
+    // var_dump($already_tweeted);
     
     $catalina_search = $connection->get("search/tweets", ["q" => "Catalina Island", "result_type" => "recent", "count" => 20]);
     $catalina_tweets = get_object_vars($catalina_search);
     $num_of_tweets = count($catalina_tweets["statuses"]);
     // echo $num_of_tweets;
+    $new_ids = [];
 
     for ($i = 0; $i < $num_of_tweets; $i++) {
-        // $tweet_ids = get_object_vars(get_object_vars($catalina_search)["statuses"][$i])["id"];
-        // echo $tweet_ids . "\n";
-        echo $i . "\n";
-        // $favorites = $connection->post("favorites/create", ["id" => "$tweet_ids"]);
-        // $retweet = $connection->post("statuses/retweet/$tweet_ids");
+        $tweet_ids = get_object_vars(get_object_vars($catalina_search)["statuses"][$i])["id"];
+        array_push($new_ids, $tweet_ids);
     }
 
-    // var_dump(get_object_vars(get_object_vars($catalina_search)["statuses"][0])["id"]);
-    // echo($catalina_search)
-    // $status = $connection->post("statuses/update", array("status" => "TEST"));
+    $new_ids = array_reverse($new_ids);
+    // var_dump($new_ids);
 
-    // var_dump($status);//print the status from twitter to the console
-
+    for ($i = 0; $i < count($new_ids); $i++) {
+        if (!in_array($new_ids[$i], $already_tweeted)) {
+            $retweet_id = $new_ids[$i];
+            $favorites = $connection->post("favorites/create", ["id" => "$tweet_ids"]);
+            $retweet = $connection->post("statuses/retweet/$retweet_id");
+            var_dump($retweet_id);
+        }
+    }
 ?>
